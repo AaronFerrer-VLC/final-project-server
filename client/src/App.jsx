563a9f81f3d2c6869f2ed162c5f9527e25aab363
Lucas Lelieur <lucas.lelieur.ll@gmail.com>
2024-12-02 12:49:36 +0100

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,9 @@
-
 const App = () => {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>HOLAAAA</h1>
+        </div>
+    )
 }
 
 export default App
